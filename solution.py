0f56e0d96b12d8df231b079538f4486e7587c0f2
Giulio Vaccari <giulio09@marcov.it>
2021-11-08 14:03:02 +0100

    First commit

---
+++ solution.py
@@ -0,0 +1,112 @@
+import io
+from typing import Optional
+
+import cv2
+import gym
+import numpy as np
+from gym.spaces import Box
+from stable_baselines3 import PPO
+from PIL import Image
+
+from wrappers import SBWrapper
+
+from aido_schemas import (
+    Context,
+    DB20Commands,
+    DB20Observations,
+    EpisodeStart,
+    JPGImage,
+    LEDSCommands,
+    logger,
+    no_hardware_GPU_available,
+    protocol_agent_DB20,
+    PWMCommands,
+    RGB,
+    wrap_direct,
+)
+
+
+class StableBaselineAgent:
+
+    def __init__(self, load_model: bool, model_path: Optional[str]):
+        self.load_model = load_model
+        self.model_path = model_path
+
+    def init(self, context: Context):
+        self.check_gpu_available(context)
+        logger.info("StableBaselineAgent init")
+        from stable_baselines3 import PPO
+
+        self.preprocessor = SBWrapper()
+        if self.model_path:
+            self.model = PPO.load(self.model_path)
+        else:
+            self.model = PPO.load("model")
+        self.current_image = np.zeros((640, 480, 3))
+
+        logger.info("Model loaded.")
+
+    def check_gpu_available(self, context: Context):
+        import torch
+
+        available = torch.cuda.is_available()
+        context.info(f"torch.cuda.is_available = {available!r}")
+        context.info("init()")
+        if available:
+            i = torch.cuda.current_device()
+            count = torch.cuda.device_count()
+            name = torch.cuda.get_device_name(i)
+            context.info(f"device {i} of {count}; name = {name!r}")
+        else:
+            no_hardware_GPU_available(context)
+
+    def on_received_seed(self, data: int):
+        np.random.seed(data)
+
+    def on_received_episode_start(self, context: Context, data: EpisodeStart):
+        context.info(f'Starting episode "{data.episode_name}".')
+
+    def on_received_observations(self, data: DB20Observations):
+        camera: JPGImage = data.camera
+        obs = jpg2rgb(camera.jpg_data)
+        self.current_image = self.preprocessor.preprocess(obs)
+
+    def compute_action(self, observation):
+        observation = self.preprocessor.preprocess(observation)
+        action, _ = self.model.predict(observation)
+        return action.astype(float)
+
+    def on_received_get_commands(self, context: Context):
+        pwm_left, pwm_right = self.compute_action(self.current_image)
+
+        pwm_left = float(np.clip(pwm_left, -1, +1))
+        pwm_right = float(np.clip(pwm_right, -1, +1))
+
+        grey = RGB(0.0, 0.0, 0.0)
+        led_commands = LEDSCommands(grey, grey, grey, grey, grey)
+        pwm_commands = PWMCommands(motor_left=pwm_left, motor_right=pwm_right)
+        commands = DB20Commands(pwm_commands, led_commands)
+        context.write("commands", commands)
+
+    def finish(self, context: Context):
+        context.info("finish()")
+
+
+def jpg2rgb(image_data: bytes) -> np.ndarray:
+    """Reads JPG bytes as RGB"""
+
+    im = Image.open(io.BytesIO(image_data))
+    im = im.convert("RGB")
+    data = np.array(im)
+    assert data.ndim == 3
+    assert data.dtype == np.uint8
+    return data
+
+def main():
+    node = StableBaselineAgent(load_model=False, model_path=None)
+    protocol = protocol_agent_DB20
+    wrap_direct(node=node, protocol=protocol)
+
+
+if __name__ == "__main__":
+    main()
---
+++ wrappers.py
@@ -0,0 +1,109 @@
+import cv2
+import gym
+import numpy as np
+
+from gym.spaces import Box
+
+cutted_img_height = 350 #@param {type: "slider", min: 0, max: 480, step:1}
+resize_ratio = 0.35 #@param {type: "slider", min: 0.0, max: 1.0, step:0.01}
+
+img_height = 480
+top_crop = img_height - cutted_img_height
+
+img_final_height = int(cutted_img_height * resize_ratio)
+img_final_width = int(640 * resize_ratio)
+
+def cropimg(img):
+    """
+    Crop top of image top_crop px, they are noise most of the time
+
+    :param img: (RGB image as np array) Image to be cropped
+    """
+    return img[top_crop:,:]
+
+def houghtransform(img):
+    """
+    Apply Hough Line transform, for theory see:
+    https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_houghlines/py_houghlines.html
+
+    :param img: (RGB image as np array)
+    """
+    frame_BGR = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    #gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY, 3)
+    edges = cv2.Canny(frame_BGR,50,150,apertureSize = 3)
+    #minLineLength = 100
+    #maxLineGap = 10
+    #lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap)
+    #for x1,y1,x2,y2 in lines[0]:
+    #    cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
+    imgRGB = cv2.cvtColor(edges, cv2.COLOR_BGR2RGB)
+    return imgRGB
+
+def resizeimg(img, ratio):
+    """
+    Resize image
+    :param img: (np array)
+    :param ratio: (float) 0<ratio<1
+    """
+    return cv2.resize(img, (0,0), fx=ratio, fy=ratio) 
+  
+def takeyellow(img):
+    """
+    Extract yellow lines, for color ranges see:
+    https://stackoverflow.com/questions/48109650/how-to-detect-two-different-colors-using-cv2-inrange-in-python-opencv
+
+    :param img: (RGB image as np array)
+    """
+    frame_HSV = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    frame_threshold = cv2.inRange(frame_HSV, (20,100,100), (50, 255, 255))
+    imgRGB = cv2.cvtColor(frame_threshold, cv2.COLOR_GRAY2RGB)
+    return imgRGB
+
+def takewhiteyellow(img):
+    """
+    Extract white and yellow lines
+
+    :param img: (RGB image as np array)
+    """
+    #white
+    sensitivity = 100
+    lower_white = np.array([0,0,255-sensitivity])
+    upper_white = np.array([255,sensitivity,255])
+    frame_HSV = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    maskwhite = cv2.inRange(frame_HSV, lower_white, upper_white)
+    img[maskwhite > 0] = (255, 0, 0)
+    img[maskwhite == 0] = (0,0,0)
+    #yellow
+    maskyellow = cv2.inRange(frame_HSV, (15,70,70), (50, 255, 255))
+    img[maskyellow > 0] = (0, 255, 0)
+    return img
+
+def white_balance(img):
+    """
+    Grayworld assumption:
+    https://stackoverflow.com/questions/46390779/automatic-white-balancing-with-grayworld-assumption/46391574
+
+    :param img: (RGB image as np array)
+    """
+    result = cv2.cvtColor(img, cv2.COLOR_RGB2LAB)
+    avg_a = np.average(result[:, :, 1])
+    avg_b = np.average(result[:, :, 2])
+    result[:, :, 1] = result[:, :, 1] - ((avg_a - 128) * (result[:, :, 0] / 255.0) * 1.1)
+    result[:, :, 2] = result[:, :, 2] - ((avg_b - 128) * (result[:, :, 0] / 255.0) * 1.1)
+    result = cv2.cvtColor(result, cv2.COLOR_LAB2RGB)
+    return result
+
+class SBWrapper:
+    def __init__(self):
+        pass
+    
+    def preprocess(self, obs):
+        """
+        Preprocess the observation
+        """
+        cropped = cropimg(obs)
+        resized = resizeimg(cropped, resize_ratio)
+        balanced = white_balance(resized)
+        extracted_colors = takewhiteyellow(balanced)
+        img = extracted_colors/255
+        return img
